f5c9cb5217a2093ca71a2ba96fbce8453fab7801
Zenxxan <Charlesmykolawit@gmail.com>
2025-08-28 18:38:50 +0800

    Delete genkey.js

--- genkey.js
+++
@@ -1,6 +0,0 @@
-
-export default function handler(req, res) {
-  const key = "OMEGA-" + Math.random().toString(36).substr(2, 6).toUpperCase();
-  global.latestKey = key;
-  res.status(200).json({ key });
-}
